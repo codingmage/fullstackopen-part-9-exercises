b0d2a277d43de3396945d1636ed1ba00314009fb
Jean Fellipe <jeanfellipesilva93@gmail.com>
2024-02-05 15:30:11 -0300

    finish exercise 9.5

--- index.ts
+++ index.ts
@@ -4,34 +4,33 @@ import { calculateBmi } from './bmiCalculator';
 const app = express();
 
 app.get('/hello', (_req, res) => {
-    res.send("Hello Full Stack")
+    res.send("Hello Full Stack");
 })
 
 app.get('/bmi', (req, res) => {
 
     try {
-        const height = Number(req.query.height)
+        const height = Number(req.query.height);
 
-        const weight = Number(req.query.weight)
+        const weight = Number(req.query.weight);
     
         if (isNaN(height) || isNaN(weight) || !height || !weight) {
-            throw new Error("malformatted parameters")
+            throw new Error("malformatted parameters");
         }
 
-        const bmi = calculateBmi(height, weight)
+        const bmi = calculateBmi(height, weight);
 
-        res.json({ weight, height, bmi})
+        res.json({ weight, height, bmi});
         
     } catch (error: unknown) {
         if (error instanceof Error) {
           console.log(error.message);
-          res.status(400).json({error: error.message})
+          res.status(400).json({error: error.message});
         }
     }
-
 })
 
-const PORT = 3003
+const PORT = 3003;
 
 app.listen(PORT, () => {
     console.log(`Now running on port: ${PORT}`)
