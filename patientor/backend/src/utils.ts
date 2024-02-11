abdc75eaa423e6fec4c3de80a6f2f92948eaf713
Jean Fellipe <jeanfellipesilva93@gmail.com>
2024-02-11 00:17:16 -0300

    finish exercises 9.12, 9.13

--- patientor/backend/src/utils.ts
+++ patientor/backend/src/utils.ts
@@ -8,6 +8,30 @@ const isGender = (param: string): param is Gender => {
 	return Object.values(Gender).map(v => v.toString()).includes(param);
 };
 
+/* const parseName = (name: unknown): string => {
+
+}
+
+const parseDateOfBirth = (dob: unknown): string => {
+
+}
+
+const parseOccupation = (occupation: unknown): string => {
+
+}
+
+const parseSSN = (ssn: unknown): string => {
+
+} */
+
+const parseDetail = (detail: unknown): string => {
+	if (!detail || !isString(detail)) {
+		throw new Error("Invalid data or missing field.");
+	}
+
+	return detail;
+};
+
 const parseGender = (gender: unknown): Gender => {
 	if(!isString(gender) || !isGender(gender)) {
 		throw new Error("Gender missing or unavailable");
@@ -17,7 +41,23 @@ const parseGender = (gender: unknown): Gender => {
 };
 
 const toNewPatient = (object: unknown) : NewPatient => {
+	if (!object || typeof object !== "object") {
+		throw new Error("Incorrect or missing data");
+	}
 
+	if ("name" in object && "dateOfBirth" in object && "occupation" in object && "gender" in object && "ssn" in object) {
+		const newPerson: NewPatient = {
+			name: parseDetail(object.name),
+			dateOfBirth: parseDetail(object.dateOfBirth),
+			gender: parseGender(object.gender),
+			occupation: parseDetail(object.occupation),
+			ssn: parseDetail(object.ssn)
+		};
+
+		return newPerson;
+	}
+
+	throw new Error("Incorrect data: some fields are missing");
 };
 
 export default toNewPatient;
