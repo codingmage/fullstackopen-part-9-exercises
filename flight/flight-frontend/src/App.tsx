c3fd747c7d33bf9eaf4d9908859b87e8a6e38eea
Jean Fellipe <jeanfellipesilva93@gmail.com>
2024-02-20 20:06:34 -0300

    add exercise 9.19

--- flight/flight-frontend/src/App.tsx
+++ flight/flight-frontend/src/App.tsx
@@ -8,8 +8,8 @@ import axios from "axios";
 function App() {
 	const [entries, setEntries] = useState<DiaryEntry[]>();
 	const [date, setDate] = useState<string>("");
-	const [visibility, setVisibility] = useState<string>("");
-	const [weather, setWeather] = useState<string>("");
+	const [visibility, setVisibility] = useState<keyof typeof Visibility>();
+	const [weather, setWeather] = useState<keyof typeof Weather>();
 	const [comment, setComment] = useState<string>("");
 	const [error, setError] = useState<string>("");
 
@@ -21,26 +21,39 @@ function App() {
 		};
 
 		void fetchEntries();
+
 	}, []);
+
+	const visibilityKeys = Object.keys(Visibility);
+
+	const weatherKeys = Object.keys(Weather);
 
 	const createEntry = async (event: React.SyntheticEvent) => {
 		event.preventDefault();
-		try {
-			const addedEntry = await entryService.addEntry({date, comment,visibility: visibility as Visibility, weather: weather as Weather});
-			// setEntries([...entries!, addedEntry]);
-			const withEntry = entries?.concat(addedEntry);
-			setEntries(withEntry);
-		} catch (error) {
-			if (axios.isAxiosError(error)) {
-				setError(error.response?.data as string);
-				setTimeout(() => {
-					setError("");
-				}, 5000);
-			} else {
-				setError("An unknown error has occurred.");
-				setTimeout(() => {
-					setError("");
-				}, 5000);
+
+		if (!date || !comment || !visibility || !weather ) {
+			setError("Some fields are missing.");
+			setTimeout(() => {
+				setError("");
+			}, 3000);
+		} else {
+			try {
+				const addedEntry = await entryService.addEntry({date, comment, visibility: visibility as Visibility, weather: weather as Weather});
+				// setEntries([...entries!, addedEntry]);
+				const withEntry = entries?.concat(addedEntry);
+				setEntries(withEntry);
+			} catch (error) {
+				if (axios.isAxiosError(error)) {
+					setError(error.response?.data as string);
+					setTimeout(() => {
+						setError("");
+					}, 5000);
+				} else {
+					setError("An unknown error has occurred.");
+					setTimeout(() => {
+						setError("");
+					}, 5000);
+				}
 			}
 		}
 	};
@@ -49,11 +62,27 @@ function App() {
 		<div>
 			<div>
 				<h3>Add new entries: </h3>
-				{ error ? <span>{error}</span> : "" }
+				{ error ? <span className="errorMessage">{error}</span> : "" }
 				<form onSubmit={createEntry}>
-					<div> <label>date</label> <input value={date} onChange={(e) => setDate(e.target.value)} /> </div>
-					<div> <label>visibility</label> <input value={visibility} onChange={(e) => setVisibility(e.target.value)}/> </div>
-					<div> <label>weather </label> <input value={weather} onChange={(e) => setWeather(e.target.value)}/> </div>
+					<div> <label>date</label> <input type="date" value={date} onChange={(e) => setDate(e.target.value)} /> </div>
+					<div className="options"> 
+						<label>visibility</label> 
+						{ visibilityKeys.map((visibility) => (
+							<div key={visibility} className="options">
+								<label>{visibility}</label>
+								<input type="radio" name="visibility" aria-label={visibility} id={visibility} value={visibility} onChange={(e) => setVisibility(e.target.value.toLowerCase() as keyof typeof Visibility)} />
+							</div>
+						))}
+					</div>
+					<div className="options"> 
+						<label>weather </label> 
+						{ weatherKeys.map((weather) => (
+							<div key={weather} className="options">
+								<label>{weather}</label>
+								<input type="radio" name="weather" aria-label={weather} id={weather} value={weather} onChange={(e) => setWeather(e.target.value.toLowerCase() as keyof typeof Weather)} />
+							</div>
+						))}
+					</div>
 					<div> <label>comment</label> <input value={comment} onChange={(e) => setComment(e.target.value)}/> </div>
 					<button type='submit'>add</button>
 				</form>
